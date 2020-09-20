39cabb261bb552d6420440a657150b2c5b05e584
Javier Centeno <fjcenteno99@gmail.com>
2020-09-19 18:57:06 -0700

    hola

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,7 +14,7 @@ const LoginForm = ()=>{
                     <div className="form-group text-left">
                         <label for="pwd">Password</label>
                         <input id="pwd" className="form-control" type="password"></input>
-                        <small id="olvidada" class="form-text text-light">Olvidaste tu contraseña?</small>
+                        <small id="olvidada" class="form-text text-light"><a href="#">¿Olvidaste tu contraseña?</a></small>
                     </div>
                     <button className="btn btn-lg btn-warning my-4">Login</button>
                     
