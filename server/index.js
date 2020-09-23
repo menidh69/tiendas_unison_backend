c63158ac95e84741375aacbb224063da3e92eec4
Manuel Duarte <manuelduarteh@gmail.com>
2020-09-22 20:57:03 -0700

    Añadimos rutas para la API GET y POST universidades

--- server/index.js
+++ server/index.js
@@ -14,6 +14,31 @@ app.get('/', (req,res)=>{
     res.send('Hola')
 })
 
+app.get("/api/v1/universidades", async (req, res)=>{
+    try{
+        const allUniversidades = await pool.query(
+            "SELECT * FROM universidad")
+        res.json(allUniversidades.rows)
+    }catch(err){
+        console.error(err.message)
+    }
+});
+
+app.post("/api/v1/universidades", async (req, res)=>{
+    try{
+        const {nombre} = req.body;
+        const {ciudad} = req.body;
+        const {estado} = req.body;
+        console.log(req.body)
+        const newUni = await pool.query(
+            "INSERT INTO universidad (nombre, ciudad, estado) VALUES($1,$2,$3) RETURNING *",
+            [nombre, ciudad, estado]
+            )
+        res.send('exito')
+    }catch(err){
+        console.error(err.message)
+    }
+});
 
 
 
