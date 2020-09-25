c0000843454edb8d24de24421f02df4810710c18
Manuel Duarte <manuelduarteh@gmail.com>
2020-09-24 17:59:44 -0700

    Ajustes a landing y registro de universidad

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,36 +1,55 @@
 import React, {Fragment} from 'react'
 import './LandingPage.css';
+import { Link } from 'react-router-dom';
 
 const LandingPage = ()=> {
     return(
         <Fragment>
         <div class = "row">
 
-            <div class = "lpleft">
+            <div class = "lpleft col-sm-5">
                  <h1>Tiendas</h1>
                  <h1>Universitarias</h1>
-                 <div class = "badg">
-                 <div class="alert alert-info" role="alert"> ¿No encuentras tu Universidad?     <a href="RegistroUniversidad" class="alert-link">¡Regístrala aquí!</a></div>
+                 <Link to="/registrouniversidad">
+                 <div class = "badge">
+                 <div class="alert alert-info" role="alert"> ¿No encuentras tu Universidad?     <span class="alert-link">¡Regístrala aquí!</span></div>
                  </div>
+                 </Link>
             </div>
 
-            <div class = "lpright">
+            <div className= "lpright col-sm-7">
                 <div className="container LandingPage">
                 <div className="form-group text-left">
-                    <div class = "ini1">
-                        <label for="email">Email address</label>
-                        <input className="form-control-sm" id="email" type="text"></input>
-                        <label for="pwd">Password</label>
-                        <input id="pwd" className="form-control-sm" type=" password "></input>
-                        <button className="btn btn-outline-warning btn-sm "><a href="#">Iniciar sesión</a></button>
+                    <form class = "ini1">
+                        <div className="row">
+                            <div className="col-md-3 mx-auto">
+                                <div className="form-group">
+                                <label for="email" className="mx-1">Email address: </label>
+                                <input className="form-control-sm" id="email" type="text"></input>
+                                </div>
+                            </div>
+                            <div className="col-md-3 mx-auto">
+                                <div className="form-group">
+                                <label for="pwd" className="mx-1">Password: </label>
+                                <input id="pwd" className="form-control-sm" type="password"></input>
+                                <small id="olvidada" class="form-text .bg-black"><a href="olvidarcontra">¿Olvidaste tu contraseña?</a></small>
+                                </div>
+                            </div>
+                            <div className="col-md-3 mx-auto">
+                            <button className="btn btn-outline-warning btn-block my-4"><a href="#">Iniciar sesión</a></button>
+                            </div>
                         </div>
-                        <div class= "ini2"><small id="olvidada" class="form-text .bg-black"><a href="olvidarcontra">¿Olvidaste tu contraseña?</a></small></div>
+                    </form>
                 </div>
-                    <div class= "ini3">
-                    <h1>Olvídate del efectivo,</h1>
-                    <h1>compralo en línea.</h1>
-                    <button className="btn btn-outline-warning btn-lg "><a href="Login">Iniciar sesión</a></button>
-                    <button className="btn btn-lg btn-warning"><a href="RegistroGeneral">Registrate</a></button>
+                    <div className= "ini3 text-center my-5">
+                        <div className="container py-5">
+                        <h1>Olvídate del efectivo,</h1>
+                        <h1>compralo en línea.</h1>
+                        <Link to="/login">
+                        <button className="btn btn-outline-warning btn-lg mx-4"><a href="Login">Iniciar sesión</a></button>
+                        </Link>
+                        <button className="btn btn-lg btn-warning mx-4"><a href="RegistroGeneral">Registrate</a></button>
+                    </div>
                 </div>
                 </div>
            
