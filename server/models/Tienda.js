da8e8d6171a6d4b3e2f5dbdabb698dec726cbec2
Manuel Duarte <manuelduarteh@gmail.com>
2020-10-26 10:22:50 -0700

    Arreglado el img-upload

--- server/models/Tienda.js
+++ server/models/Tienda.js
@@ -27,9 +27,9 @@ module.exports = db.sequelize.define(
         tarjeta: {
             type: Sequelize.BOOLEAN
         },
-        // validada: {
-        //     type: Sequelize.BOOLEAN
-        // },
+        validada: {
+            type: Sequelize.BOOLEAN
+        },
         fechaSub: {
           type: Sequelize.DATE
         },
