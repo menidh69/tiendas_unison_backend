df80a8b2ba5134ad79d0842927102a5e04b98c04
anaquirartea <anaquirartea@gmail.com>
2020-10-26 18:50:59 -0700

    reportes

--- client/src/components/Cliente/TiendaInfo.js
+++ client/src/components/Cliente/TiendaInfo.js
@@ -59,6 +59,33 @@ const TiendaInfo = () => {
         <Link to={`/tiendas/${id}/menu`}>
         <button className="mx-auto text-center btn btn-lg rounded-pill btn-primary"> Ver menú </button>
         </Link>
+
+        <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#myModal">Reportar tienda</button>
+
+                            <div id="Modal" class="modal">
+                         <div class="modal-dialog">
+                         <div class="modal-content">
+                         <div class="modal-header">
+                          
+                            <h4 class="modal-title">Reporte de tienda</h4>
+                        </div>
+                        <div class="modal-body">
+                        <p>En caso de que la tienda no exista, ayúdanos con tu reporte para evitar fraudes, al enviarlo estás afirmando que la tienda no existe </p>
+                        <div class="form-group">
+
+                        <div class="custom-control custom-checkbox mr-sm-2">
+                        <input type="checkbox" required class="custom-control-input" id="customControlAutosizing"/>
+                        <label class="custom-control-label" for="customControlAutosizing">Ok, entiendo.</label>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                            <button type="button" class="btn btn-primary" onClick={()=>reportar()} data-dismiss="modal" >Enviar reporte</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
     
   </Fragment>)
