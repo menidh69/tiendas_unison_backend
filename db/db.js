79bcf73ecaa8f60e7b6867c2ddb637b3776ea10d
Manuel Duarte <manuelduarteh@gmail.com>
2020-12-04 01:05:21 -0700

    changed db environment var

--- db/db.js
+++ db/db.js
@@ -7,7 +7,7 @@ const db = {}
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
 
-    host: 'localhost',
+    host: process.env.HOST,
     dialect: 'mysql',
     operatorsAliases: '0',
 
