6d029d84ff3680660939018f0c4a3316be74b7f4
Manuel Duarte <manuelduarteh@gmail.com>
2020-12-04 01:30:29 -0700

    Changed build location

---
+++ build/precache-manifest.2e252608265f06917bfc01350670eab5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31aa4663d22515a9371b7003cb7c45e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b1b0dd42d0f9afaf73",
+    "url": "/static/css/2.6821bcd3.chunk.css"
+  },
+  {
+    "revision": "5b7454aa39db4dc7092e",
+    "url": "/static/css/main.b37b5eb2.chunk.css"
+  },
+  {
+    "revision": "c4b1b0dd42d0f9afaf73",
+    "url": "/static/js/2.901951ba.chunk.js"
+  },
+  {
+    "revision": "10028160301d7973c3256d37ce5a2c60",
+    "url": "/static/js/2.901951ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7454aa39db4dc7092e",
+    "url": "/static/js/main.da7d4f15.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ed7d9081e1f1d47acb0b3d0031afc501",
+    "url": "/static/media/cerrarsesion.ed7d9081.svg"
+  },
+  {
+    "revision": "ec23fc06cdf77cf62f89960ac373e787",
+    "url": "/static/media/configuracion.ec23fc06.svg"
+  },
+  {
+    "revision": "76d2151c06b0cc6c0ba5be5318b66836",
+    "url": "/static/media/dropdown.76d2151c.svg"
+  },
+  {
+    "revision": "d646815664bb127849064f0068f73a8a",
+    "url": "/static/media/home.d6468156.svg"
+  },
+  {
+    "revision": "45e491e51167ce9337961cc7580f324a",
+    "url": "/static/media/panel.45e491e5.svg"
+  },
+  {
+    "revision": "2b6d8f7bfc777686f209c1fc475db0da",
+    "url": "/static/media/shoppingcart.2b6d8f7b.svg"
+  }
+]);
