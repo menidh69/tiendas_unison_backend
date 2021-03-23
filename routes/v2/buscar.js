43f062dd07e4bcde54bf0e6d909b9631aa135fcf
Manuel Duarte <manuelduarteh@gmail.com>
2021-03-22 19:01:16 -0700

    Se agregó ruta de busqueda producto

---
+++ routes/v2/buscar.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {Carrito, Carrito_item, Productos} = require('../../models/entities')
+
+router.get('/search', async (req, res) => {
+
+    //Holds value of the query param 'searchquery'.
+      const searchQuery = req.query.producto;
+  
+    //Do something when the searchQuery is not null.
+    if(searchQuery != null){
+        let productos = Productos.findAll({where:{nombre: searchQuery}})
+        return res.json({productos: productos})
+    }else{
+      res.end();
+    }
+  });
+
+module.exports = router;
